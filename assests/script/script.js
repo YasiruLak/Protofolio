5ec7502657f66ccc3db690daa9c28c1cb5ec65c2
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-04-03 23:09:25 +0530

    updated

--- assests/script/script.js
+++ assests/script/script.js
@@ -5,8 +5,25 @@ $(document).ready(function (){
         }else{
             $('.navbar').removeClass("sticky");
         }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
+
+
+    $('.scroll-up-btn').click(function (){
+        $('html').animate({scrollTop: 0});
+    });
+
     // toggle menu/navbar script
+
+
+
+
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
