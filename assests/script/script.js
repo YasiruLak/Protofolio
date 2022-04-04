5501cd7c759e6caaec0734409e53534d502a0fb7
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-04-04 22:25:57 +0530

    gallery done

--- assests/script/script.js
+++ assests/script/script.js
@@ -30,7 +30,7 @@ $(document).ready(function (){
 
     });
 
-    var typed = new Typed(".typing-2",{
+    var typed = new Typed(".",{
         strings: ["Full Stack Developer", "Freelance", "Developer", "Designer", "Blogger", "Traveller"],
         typeSpeed: 100,
         backSpeed: 60,
@@ -67,3 +67,28 @@ $(document).ready(function (){
         }
     });
 });
+
+// selecting all required element
+
+const  filterItem = document.querySelector(".items");
+const  filterImg = document.querySelectorAll(".image1");
+
+window.onload = () =>{
+    filterItem.onclick = (selectedItem)=>{
+    if (selectedItem.target.classList.contains("item")){
+        filterItem.querySelector(".active").classList.remove("active");
+        selectedItem.target.classList.add("active");
+        let filterName = selectedItem.target.getAttribute("data-name");
+            filterImg.forEach((image1)=>{
+                let filterImages = image1.getAttribute("data-name");
+                if ((filterImages == filterName) || filterName == "all"){
+                    image1.classList.remove("hide");
+                    image1.classList.add("show");
+                }else{
+                    image1.classList.add("hide");
+                    image1.classList.remove("show");
+                }
+            });
+        }
+    }
+}
