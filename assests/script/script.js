220f040fe8195f7643cdc20a72cc6dc266e655d8
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-04-04 11:57:05 +0530

    updated

--- assests/script/script.js
+++ assests/script/script.js
@@ -14,19 +14,35 @@ $(document).ready(function (){
         }
     });
 
+    // slid up script
 
     $('.scroll-up-btn').click(function (){
         $('html').animate({scrollTop: 0});
     });
 
+    // typing animation script
+
+    var typed = new Typed(".typing",{
+        strings: ["Full Stack Developer", "Freelance", "Developer", "Designer", "Blogger", "Traveller"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+
+    });
+
+    var typed = new Typed(".typing-2",{
+        strings: ["Full Stack Developer", "Freelance", "Developer", "Designer", "Blogger", "Traveller"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+
+    })
     // toggle menu/navbar script
-
-
-
 
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+        $('.scroll-up-btn').removeClass("show");
     });
 
     $('.carousel').owlCarousel({
